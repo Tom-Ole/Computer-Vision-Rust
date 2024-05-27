0e8698abf748f0a5a00835541c7b417f5e7f821a
stu240823 <stu240823@mail.uni-kiel.de>
2024-05-27 16:30:17 +0200

    :)

--- src/client/app.js
+++ src/client/app.js
@@ -2,10 +2,10 @@ let image_input = document.querySelector("#img-input");
 
 let originalImage = document.querySelector("#original")
 let imageContainer = document.querySelector("#img-container")
-
+let original_src;
 
 let id = 1000
-function createImage(base64, label="") {
+function createImage(base64, label="", container) {
 
     let d = new Date();
     let time = d.toLocaleTimeString();
@@ -17,60 +17,69 @@ function createImage(base64, label="") {
 
     let img = document.createElement("img")
     img.src = `data:image/png;base64,${base64}`
-    imageContainer.append(imageLabel)
-    imageContainer.append(img)
+    container.append(imageLabel)
+    container.append(img)
     return true;
 }
 
 image_input.addEventListener("change", async () => {
     if(image_input.files.length > 0) {
+
+        let imageBox = document.createElement("div")
+
+
         let file = image_input.files[0]
         let imageData = new FormData()
         imageData.append("file", file);
 
         let reader = new FileReader();
         reader.onload = (evt) => {
-            originalImage.src = evt.target.result
+            originalImage.src = evt.target.result;
+            let d = new Date();
+            let time = d.toLocaleTimeString()
+    
+            let imageLabel = document.createElement("label")
+            imageLabel.for = id
+            id++
+            imageLabel.innerText = "Original " + "(" + time + "):"
+            let copy_current = document.querySelector("#original").cloneNode(true);
+            copy_current.id = id
+            copy_current.src = evt.target.result
+            imageBox.append(imageLabel);
+            imageBox.append(copy_current);
         }
         reader.readAsDataURL(file)
 
-        let imageLabel = document.createElement("label")
-        imageLabel.for = id
-        id++
-        let copy_current = document.querySelector("#original").cloneNode(true);
-        copy_current.id = id
-        imageContainer.append(copy_current);
         
         let selectedAlgo = document.querySelector("#algo").value
 
         let sigma = document.querySelector("#sigma").value;
         let threshold = document.querySelector("#threshold").value;
 
-        fetchImage(file, selectedAlgo, sigma, threshold)
+        await fetchImage(file, selectedAlgo, sigma, threshold, imageBox)
+        imageContainer.prepend(imageBox)
 
     }
 })
 
 
-async function fetchImage(image, algo, sigma, threshold) {
+async function fetchImage(image, algo, sigma, threshold, container) {
 
     switch(algo) {
         case "canny": 
             let data1 = await canny(image, sigma, threshold)
-            return createImage(data1.data.base64, algo)
+            return createImage(data1.data.base64, algo, container)
         case "sobel":
             let data2 = await sobel(image, sigma);
-            return createImage(data2.data.base64, algo)
+            return createImage(data2.data.base64, algo, container)
         case "harris":
             let data3 = await harris(image);
-            return createImage(data3.data.base64, algo)
+            return createImage(data3.data.base64, algo, container)
         case "all":
-            let img1 = await canny(image, sigma, threshold)
-            createImage(img1.data.base64, "Canny")
-            let img2 = await sobel(image, sigma)
-            createImage(img2.data.base64, "Sobel")
-            let img3 = await harris(image, sigma)
-            createImage(img3.data.base64, "Harris")
+            let data = await all(image, sigma, threshold)
+            createImage(data.data.canny, "Canny", container)
+            createImage(data.data.sobel, "Sobel", container)
+            createImage(data.data.harris, "Harris", container)
             return true
         default: 
             return null;
@@ -145,4 +154,23 @@ async function harris(image) {
     })
 
     return await res.json();
+}
+
+async function all(image, sigma, threshold) {
+    let r1 = await setSigma(sigma)
+    let r2 = await setThreshold(threshold)
+
+    if(r1.ok && r2.ok) {
+        let res = await fetch("/all", {
+            method: "POST",
+            headers: {
+                "Content-length": image.length
+            },
+            body: image
+        });
+        
+        return await res.json()
+    } else {
+        return null
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-
 /*
 ################################################################################
 # Reference: https://en.wikipedia.org/wiki/Feature_(computer_vision)#Detectors #
 ################################################################################
 */
 
-mod gausian_blur;
 mod canny;
-mod sobel;
+mod gausian_blur;
 mod harris;
+mod sobel;
 
 use std::{
-    str, fs, io::{BufRead, BufReader, Read, Write}, net::{TcpListener, TcpStream}, time::Instant
+    fs,
+    io::{BufRead, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+    str,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+    time::Instant,
 };
 
 use base64::{engine::general_purpose::STANDARD, Engine};
@@ -19,14 +24,14 @@ use canny::canny;
 use serde_json::{json, Value};
 
 use crate::{harris::harris, sobel::sobel};
-
+#[derive(Clone)]
 struct InputValues {
     sigma: f32,
     threshold: f32,
 }
 
 fn main() {
-    let mut values = InputValues  {
+    let mut values = InputValues {
         sigma: 1.0,
         threshold: 0.3,
     };
@@ -41,11 +46,7 @@ fn main() {
     }
 }
 
-
 fn handle_connection(mut stream: TcpStream, values: &mut InputValues) {
-
-
-
     let mut buf_reader = BufReader::new(&mut stream);
     let mut request_line = "".to_string();
     buf_reader.read_line(&mut request_line).unwrap();
@@ -64,77 +65,125 @@ fn handle_connection(mut stream: TcpStream, values: &mut InputValues) {
         headers.push(header_line.clone());
     }
 
-    let content_length = headers.iter()
+    let content_length = headers
+        .iter()
         .filter_map(|header| {
             if header.to_lowercase().starts_with("content-length") {
-                header.split_whitespace().nth(1).and_then(|v| v.trim().parse::<usize>().ok())
+                header
+                    .split_whitespace()
+                    .nth(1)
+                    .and_then(|v| v.trim().parse::<usize>().ok())
             } else {
                 None
             }
         })
         .next()
         .unwrap_or(0);
-    
+
     let mut body = vec![0; content_length];
     buf_reader.read_exact(&mut body).unwrap();
 
+    let (canny_tx, canny_rx) = mpsc::channel();
+    let (sobel_tx, sobel_rx) = mpsc::channel();
+    let (harris_tx, harris_rx) = mpsc::channel();
+
     let response = match request_line.as_str() {
         "GET / HTTP/1.1" => {
-            let contents = fs::read_to_string("src/html/index.html").unwrap();
+            let contents = fs::read_to_string("src/client/index.html").unwrap();
             response_200(contents)
         }
         "GET /app.js HTTP/1.1" => {
-            let contents = fs::read_to_string("src/html/app.js").unwrap();
+            let contents = fs::read_to_string("src/client/app.js").unwrap();
             response_200(contents)
         }
         "GET /style.css HTTP/1.1" => {
-            let contents = fs::read_to_string("src/html/style.css").unwrap();
+            let contents = fs::read_to_string("src/client/style.css").unwrap();
             response_200(contents)
         }
         "POST /setSigma HTTP/1.1" => {
-            let t = str::from_utf8(&body).unwrap().parse::<f32>().unwrap(); 
+            let t = str::from_utf8(&body).unwrap().parse::<f32>().unwrap();
             values.sigma = t;
-            
+
             response_200("Ok".to_string())
         }
         "POST /setThreshold HTTP/1.1" => {
-            let t = str::from_utf8(&body).unwrap().parse::<f32>().unwrap(); 
+            let t = str::from_utf8(&body).unwrap().parse::<f32>().unwrap();
             values.threshold = t;
-            
+
             response_200("Ok".to_string())
         }
         "POST /canny HTTP/1.1" => {
             println!("Start Processing canny");
             let now = Instant::now();
-                let new_image = canny(&body, &values.sigma, &values.threshold);
+            let new_image = canny(&body, &values.sigma, &values.threshold);
             println!("Elapsed time: {:.2?}", now.elapsed());
-                response_json(json!({
-                    "data": {
-                        "base64": STANDARD.encode(&new_image)
-                    }
-                }))
+            response_json(json!({
+                "data": {
+                    "base64": STANDARD.encode(&new_image)
+                }
+            }))
         }
         "POST /sobel HTTP/1.1" => {
             println!("Start Processing sobel");
             let now = Instant::now();
-                let new_image = sobel(&body, &values.sigma);
+            let new_image = sobel(&body, &values.sigma);
             println!("Elapsed time: {:.2?}", now.elapsed());
-                response_json(json!({
-                    "data": {
-                        "base64": STANDARD.encode(&new_image)
-                    }
-                }))
+            response_json(json!({
+                "data": {
+                    "base64": STANDARD.encode(&new_image)
+                }
+            }))
         }
         "POST /harris HTTP/1.1" => {
             println!("Start Processing Harris");
             let now = Instant::now();
-                let new_image = harris(&body);
+            let new_image = harris(&body);
             println!("Elapsed time: {:.2?}", now.elapsed());
-                response_json(json!({
-                    "data": {
-                        "base64": STANDARD.encode(&new_image)
-                    }
-                }))
+            response_json(json!({
+                "data": {
+                    "base64": STANDARD.encode(&new_image)
+                }
+            }))
+        }
+        "POST /all HTTP/1.1" => {
+            let body_mut = Arc::new(Mutex::new(body));
+            println!("Start Processing All");
+            let now = Instant::now();
+            let body_clone_1 = Arc::clone(&body_mut);
+            let values_clone_1 = values.clone();
+            let thread_1 = thread::spawn(move || {
+                let canny_image = canny(
+                    &body_clone_1.lock().unwrap(),
+                    &values_clone_1.sigma,
+                    &values_clone_1.threshold,
+                );
+                canny_tx.send(canny_image).unwrap();
+            });
+
+            let body_clone_2 = Arc::clone(&body_mut);
+            let values_clone_2 = values.clone();
+            let thread_2 = thread::spawn(move || {
+                let sobel_image = sobel(&body_clone_2.lock().unwrap(), &values_clone_2.sigma);
+                sobel_tx.send(sobel_image).unwrap();
+            });
+
+            let body_clone_3 = Arc::clone(&body_mut);
+            let thread_3 = thread::spawn(move || {
+                let harris_image = harris(&body_clone_3.lock().unwrap());
+                harris_tx.send(harris_image).unwrap();
+            });
+            thread_1.join().unwrap();
+            thread_2.join().unwrap();
+            thread_3.join().unwrap();
+
+            println!("Elapsed time: {:.2?}", now.elapsed());
+            response_json(json!({
+                "data": {
+                    "canny": STANDARD.encode(&canny_rx.recv().unwrap()),
+                    "sobel": STANDARD.encode(&sobel_rx.recv().unwrap()),
+                    "harris": STANDARD.encode(&harris_rx.recv().unwrap())
+                }
+            }))
         }
         _ => {
             println!("Request line: {}", request_line);
@@ -167,4 +216,3 @@ fn response_json(data: Value) -> String {
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     return response;
 }
-
